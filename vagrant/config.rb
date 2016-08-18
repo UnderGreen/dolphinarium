5838d904e66537a2236ef54f9afd7df485489af0
Sergei Antipov <greendayonfire@gmail.com>
2016-08-18 18:28:08 +0700

    Added possibility to change cpu count for nodes

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -2,7 +2,8 @@ $update_channel="stable"
 
 $controller_count=1
 $controller_vm_memory=512
+$controller_vm_vcpus=2
 
-$worker_count=3
+$worker_count=1
 $worker_vm_memory=1024
-$worker_vcpus=2
+$worker_vm_vcpus=2
