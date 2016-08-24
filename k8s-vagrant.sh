7209657846f2dabc1b3300f8ca8e18d6959ae41e
Sergei Antipov <greendayonfire@gmail.com>
2016-08-24 15:34:59 +0700

    Added improvements into k8s-vagrant.sh

--- k8s-vagrant.sh
+++ k8s-vagrant.sh
@@ -7,6 +7,48 @@ PROJECT_DIR="$SCRIPT_DIR"
 
 source "$PROJECT_DIR/tools/shell-functions.sh"
 
+function vagrant_up() {
+    announce-step "Bringing up vagrant VMs"
+
+    pushd -- "$PROJECT_DIR/vagrant/"
+    vagrant up
+    popd
+}
+
+function wait_for_k8s() {
+    announce-step "Waiting for K8S"
+
+    local cmd="bash $PROJECT_DIR/vagrant/lib/cluster-status.sh"
+    wait-for-command "$cmd" 10 120
+}
+
+function k8s_cluster_check() {
+    announce-step "Deploying test services to K8S"
+
+    kubectl run nginx-test --image=nginx --port=80
+
+    # Wait pod to appear
+    local cmd='kubectl get pods -l run=nginx-test | grep ^nginx-test'
+    wait-for-command "$cmd" 10 30
+
+    # Determine pod to expose
+    local pod=$(kubectl get pods -l run=nginx-test --no-headers | awk '{ print $1 }')
+    kubectl expose pod "$pod" --target-port=80 --name=nginx-test \
+	    --type=LoadBalancer
+
+    local nginx_endpoint="http://$(k8s-service-endpoint nginx-test 80)"
+    wait-for-http "$nginx_endpoint" 5 30
+
+    kubectl delete service nginx-test
+    kubectl delete deployment nginx-test
+}
+
+function k8s_wait_for_workers() {
+    announce-step "Waiting for 3 worker nodes"
+
+    local cmd="[ $(kubectl get nodes | grep -e '^172\.17\.4\.2' | awk '{ print $1 }' | wc -l) -eq 3 ]"
+    wait-for-command "$cmd" 10 30
+}
 
 function deploy_registry() {
     announce-step "Deploying docker registry"
@@ -15,6 +57,14 @@ function deploy_registry() {
 
     local registry="http://$(k8s-service-endpoint kube-registry 5000 kube-system)"
     wait-for-http "$registry" 5 30
+}
+
+function label_worker_nodes() {
+    announce-step "Setting labels on nodes"
+    for node in `kubectl get nodes | grep -e '^172\.17\.4\.2' | awk '{ print $1 }'`
+    do
+        kubectl label node $node name=galera-cluster
+    done
 }
 
 function build_and_push_galera() {
@@ -26,7 +76,13 @@ function build_and_push_galera() {
 }
 
 function main() {
+    vagrant_up
+    wait_for_k8s
+    eval $(bash "$PROJECT_DIR/set-kubeconfig-vagrant.sh")
+    k8s_cluster_check
+    k8s_wait_for_workers
     deploy_registry
+    label_worker_nodes
     build_and_push_galera
 }
 
